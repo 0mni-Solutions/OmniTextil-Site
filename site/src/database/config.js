01faaff6c0cb02223e8fb3b4c6046d1d1c295ebb
VitorMendesco <vitor.mcosta@sptech.school>
2022-05-25 09:24:02 -0300

    conexão com a azure no config.js

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -2,20 +2,20 @@ var mysql = require("mysql2");
 var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-// var sqlServerConfig = {
-//     user: "XXXXX",
-//     password: "XXXXX",
-//     database: "XXXXX",
-//     server: "XXXXX",
-//     pool: {
-//         max: 10,
-//         min: 0,
-//         idleTimeoutMillis: 30000
-//     },
-//     options: {
-//         encrypt: true, // for azure
-//     }
-// }
+var sqlServerConfig = {
+    user: "admin-omnitextil-7",
+    password: "#Gfgrupo7",
+    database: "OmniTextil",
+    server: "svr-omnitextil-grupo7.database.windows.net",
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: true, // for azure
+    }
+}
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
